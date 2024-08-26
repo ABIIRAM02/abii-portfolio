3d99ae6004cd4dfa0de2ae3dce96fdf50a141a7b
darskp <darshanpatgar100@gmail.com>
2024-08-26 22:26:59 +0530

    project setup

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/component/navbar";
+import React, { ReactNode } from "react";
+
+function Homelayout({ children }: { children: ReactNode }) {
+    return <>
+        <Navbar />
+        {children}
+    </>;
+}
+
+export default Homelayout;
---
+++ component/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )
+}
+
+export default Navbar;
