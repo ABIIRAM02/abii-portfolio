4f03ca7f4c7ed3eddbadac6ca50d41ff73ceb80b
Darshan Kumar <60054646+darskp@users.noreply.github.com>
2024-08-30 10:57:08 +0530

    Revert "merged code"

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Work from '@/component/Work'
+import Nav from '@/component/Nav'
+
+const Home = () => {
+  return (
+    <>
+    <Nav />
+    <Work/>
+    </>
+  )
+}
+
+export default Home
